634c042f8b3fc96503436bf6c10142bf615ca4bd
stanford kangethe <stanfordkangethe16@gmail.com>
2022-08-23 12:21:44 +0300

    anagram complete

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagrams)
+    anagrams.filter do |anagram|
+      @word.chars.sort == anagram.chars.sort
+    end
+  end
+
+end
